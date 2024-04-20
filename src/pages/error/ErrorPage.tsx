abc4bc6e61d70740504496e08f4875409f7da197
romCho <saerom.r.cho@ent-bc.com>
2024-04-21 07:03:45 +0900

    feat: error 페이지 css 보완

--- src/pages/error/ErrorPage.tsx
+++ src/pages/error/ErrorPage.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { styled } from "styled-components";
 
 const ErrorPage = () => {
   const navigate = useNavigate();
@@ -8,13 +9,30 @@ const ErrorPage = () => {
   }
 
   return (
-    <div>
-      <h1>Error Occurred</h1>
-      <p>문제가 발생하였습니다. 잠시만 기다려주세요. 🙏 </p>
-      <p>문제가 계속 지속되는 경우 saerom.r.cho@gmail.com 으로 메일을 보내주세요. </p>
-      <div onClick={() => goHome()}>메인 홈으로 이동하기</div>
-    </div>
+    <>
+      <ErrorTitle>Error Occurred</ErrorTitle>
+      <ErrorContent>문제가 발생하였습니다. 잠시만 기다려주세요. 🙏 </ErrorContent>
+      <ErrorContent>문제가 계속 지속되는 경우 <a href="mailto:saerom.r.cho@gmail.com">saerom.r.cho@gmail.com</a> 으로 메일을 보내주세요. </ErrorContent>
+      <GoToHome onClick={() => goHome()} >메인 홈으로 이동하기</GoToHome>
+    </>
   );
 };
 
 export default ErrorPage;
+
+const ErrorTitle = styled.h1`
+  text-align: center;
+  margin-bottom: 5%;
+
+`;
+const ErrorContent = styled.div`
+  text-align: center;
+  margin-top: 2%;
+`;
+const GoToHome = styled.div`
+  text-align: center;
+  margin-top: 5%;
+  font-size: 20px;
+  cursor: pointer;
+`;
+
