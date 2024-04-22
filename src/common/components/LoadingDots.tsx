e11fbf6b93438f74c8d9c34ec0239a748e6e01b1
romCho <saerom.r.cho@ent-bc.com>
2024-04-22 20:20:21 +0900

    fix: loadingDots 수정, 애니메이션 수정

--- src/common/components/LoadingDots.tsx
+++ src/common/components/LoadingDots.tsx
@@ -1,29 +1,41 @@
+import { IDotProps, IPropsLoadingDotsProps } from '@common/interface/interface';
 import styled, { keyframes } from 'styled-components';
 
-const LoadingDots = () => {
-  const dotStyle = (delay: string) => ({
-    width: '10px',
-    height: '10px',
-    borderRadius: '50%',
-    backgroundColor: '#4A90E2',
-    marginRight: '5px',
-    animation: `flash 1.4s ${delay} infinite ease-in-out both`,
-  });
-
-  return (
-    <DotsContainer>
-      <div style={dotStyle('0s')} />
-      <div style={dotStyle('0.2s')} />
-      <div style={dotStyle('0.4s')} />
-    </DotsContainer>
-  );
-};
+const LoadingDots = () => (
+  <DotsContainer>
+    <Dot delay="0s" />
+    <Dot delay="0.2s" />
+    <Dot delay="0.4s" />
+  </DotsContainer>
+);
 
 export default LoadingDots;
 
-const DotsContainer = styled.div`
+const DotsContainer = styled.div<IPropsLoadingDotsProps>`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 100px;
+`;
+
+const flash = keyframes`
+    0%, 100% {
+        opacity: 0;
+    }
+    50% {
+        opacity: 1;
+    }
+`;
+
+const Dot = styled.div<IDotProps>`
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: #4a90e2;
+  animation: ${flash} 1.4s infinite ease-in-out both;
+  animation-delay: ${(props) => props.delay};
+
+  &:not(:last-child) {
+    margin-right: 5px;
+  }
 `;
