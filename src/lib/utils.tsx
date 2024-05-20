cfc4cbbe0cf8ac8dad8149bb2f09305d909a21dd
romCho <saerom.r.cho@ent-bc.com>
2024-05-20 11:13:14 +0900

    chore: 유틸함수 설명 추가

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -4,7 +4,17 @@ import { CurrencyEnum } from './enum';
 import { CurrencyText, SymbolText } from '@common/components/CoinTable';
 import CoinNameCell from '@common/components/CoinNameCell';
 import { ICoin } from '@common/interface/interface';
-
+/**
+ * 
+ "유틸성 함수"에서 "유틸성"은 "유틸리티(Utility)"의 약자로, 
+ 편리하고 유용한 기능을 제공하는 도구나 도구 모음을 의미합니다. 
+ 프로그래밍에서 "유틸성 함수"는 일반적으로 자주 사용되는 공통 작업을 수행하거나, 
+ 특정 작업을 쉽게 처리할 수 있도록 도와주는 함수들을 의미합니다. 
+ 이러한 함수들은 특정 애플리케이션의 비즈니스 로직과는 직접적인 관련이 없지만, 
+ 개발자의 생산성을 높이고 코드의 재사용성을 높이는 데 중요한 역할을 합니다.
+ 유틸성 함수는 반복적으로 사용되는 작업을 간편하게 처리할 수 있게 하여 개발 생산성을 높이고 코드의 가독성을 개선합니다. 
+ 이러한 함수들은 프로젝트 전반에 걸쳐 널리 사용되며, 코드베이스의 유지보수성을 높이는 중요한 역할을 합니다.
+ */
 export const getColumnsData = (currency: CurrencyEnum) => [
   {
     accessorKey: 'name',
